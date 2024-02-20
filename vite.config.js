9bd54f51995ee0c00867e119dbedd5d9b3a5fa31
Aniket Markam <aniketmarkam26@gmail.com>
2024-02-20 18:25:13 +0530

    updation for the deployment of the project

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Calculator/",
+  base: "",
+  build: {
+    outDir: "dist",
+  },
   plugins: [react()],
 });
